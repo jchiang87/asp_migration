01eed63be6e28fd4f1299e1a3fa5e8f9306ea1e8
James Chiang <jchiang87@gmail.com>
2023-09-20 14:11:39 -0700

    re-organize scripts

--- cvs_to_git/make_repo.sh
+++ cvs_to_git/make_repo.sh
@@ -1,8 +1,13 @@
-#python cleanup_dump_file.py > cvs2git-tmp/git-dump-clean.dat
+python clean_dump_file.py cvs2git-tmp/git-dump.dat > cvs2git-tmp/git-dump-clean.dat
+
 mkdir ASP
 cd ASP
 git init
-bash ../run_fast-import.sh
+
+git fast-import --export-marks=../cvs2git-tmp/git-marks.dat < ../cvs2git-tmp/git-blob.dat
+#git fast-import --import-marks=../cvs2git-tmp/git-marks.dat < ../cvs2git-tmp/git-dump.dat
+git fast-import --import-marks=../cvs2git-tmp/git-marks.dat < ../cvs2git-tmp/git-dump-clean.dat
+
 git checkout master
 git reset --hard
 git branch -m main
--- cvs_to_git/run_fast-import.sh
+++
@@ -1,3 +0,0 @@
-git fast-import --export-marks=../cvs2git-tmp/git-marks.dat < ../cvs2git-tmp/git-blob.dat
-git fast-import --import-marks=../cvs2git-tmp/git-marks.dat < ../cvs2git-tmp/git-dump.dat
-#git fast-import --import-marks=../cvs2git-tmp/git-marks.dat < ../cvs2git-tmp/git-dump-clean.dat
