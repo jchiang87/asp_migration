37230b224e0c0294e1b59873362a870e5a251929
James Chiang <jchiang87@gmail.com>
2023-09-20 14:33:19 -0700

    uncomment rsync command for ASP area; use mkdir for ./cvs/CVSROOT instead of rsyncing the folder

--- cvs_to_git/rsync_ASP_repo.sh
+++ cvs_to_git/rsync_ASP_repo.sh
@@ -1,2 +1,2 @@
-#rsync -avzh jchiang@cent7b.slac.stanford.edu:/nfs/slac/g/glast/ground/cvs/ASP ./cvs/
-#rsync -avzh jchiang@cent7b.slac.stanford.edu:/nfs/slac/g/glast/ground/cvs/CVSROOT ./cvs/
+rsync -avzh jchiang@cent7b.slac.stanford.edu:/nfs/slac/g/glast/ground/cvs/ASP ./cvs/
+mkdir ./cvs/CVSROOT
