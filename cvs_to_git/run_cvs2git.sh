8eea32c4acd01385155d7269a5caea4121493499
James Chiang <jchiang87@gmail.com>
2023-09-20 14:34:27 -0700

    remove unneeded options

--- cvs_to_git/run_cvs2git.sh
+++ cvs_to_git/run_cvs2git.sh
@@ -1,6 +1,4 @@
 python2 ./cvs2svn/cvs2git \
         --blobfile=cvs2git-tmp/git-blob.dat \
         --dumpfile=cvs2git-tmp/git-dump.dat \
-        --username=cvs2git \
-        --use-cvs \
         ./cvs/ASP
