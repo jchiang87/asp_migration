5a0b2006c597643ca750779bc1334e6e197642ad
James Chiang <jchiang87@gmail.com>
2023-09-25 16:13:40 -0700

    fix test for ./cvs/CVSROOT

--- cvs_to_git/rsync_cvs_repo.sh
+++ cvs_to_git/rsync_cvs_repo.sh
@@ -4,6 +4,6 @@ repo=$1
 
 rsync -avzh jchiang@cent7b.slac.stanford.edu:/nfs/slac/g/glast/ground/cvs/$1 ./cvs/
 
-if [ ! -d ./cvs/ROOT ]; then
+if [ ! -d ./cvs/CVSROOT ]; then
     mkdir ./cvs/CVSROOT
 fi
